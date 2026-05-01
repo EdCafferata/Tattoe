7f8367326eaeb57f16b0c953e470c4c70d0902f8
Ed Cafferata <edcafferata@icloud.com>
2026-05-01 21:57:38 +0200

    Initial Commit

---
+++ tattoe/tattoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tattoeApp.swift
+//  tattoe
+//
+//  Created by Ed Cafferata on 01/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct tattoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
